c1a3135acd30d57135e4c289adefdf181277c3c0
Ashraf Mohamed <41853901+nejishow@users.noreply.github.com>
2020-05-27 12:50:07 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueGoogleCharts from 'vue-google-charts'
+axios.defaults.headers.common['X-API-KEY'] = 'opFZVW2F1kO4BLDgxXxZbAIJRC2w9kayoH0WoqPo'
+Vue.use(VueAxios, axios)
+
+Vue.use(VueGoogleCharts)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,207 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div v-show="loading" class="bg-secondary text-light notification text-center">
+      データのロード....
+    </div>
+    <div class="row mt-3">
+      <div class="col-12">
+        YUMEMI CHALLENGE
+        <hr />
+      </div>
+      <div class="col-12 m-3">
+        <div class="form-check form-check-inline" v-for="(pref, index) in prefectures" :key="index">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="inlineCheckbox1"
+            :value="pref.prefCode"
+            @change="showData($event, pref)"
+          />
+          <label class="form-check-label" for="inlineCheckbox1">{{pref.prefName}}</label>
+        </div>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col-12">
+        <GChart v-show="data" type="LineChart" :data="chartData" :options="chartOptions" />
+        <div v-show="!data" class="card border-info card-body">
+          人口を見るようにをチェクください
+        </div>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col-12">
+      </div>
+    </div>
+    <div class="row footer mb-3">
+      <div class="col-12">
+        アシュラフ
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { GChart } from 'vue-google-charts'
+import axios from 'axios'
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    GChart
+  },
+  data () {
+    return {
+      // Array will be automatically processed with visualization.arrayToDataTable function
+      defaultChartData: [
+        ['Year'],
+        ['1960'],
+        ['1965'],
+        ['1970'],
+        ['1975'],
+        ['1980'],
+        ['1985'],
+        ['1990'],
+        ['1995'],
+        ['2000'],
+        ['2005'],
+        ['2010'],
+        ['2015'],
+        ['2020'],
+        ['2025'],
+        ['2030'],
+        ['2035'],
+        ['2040'],
+        ['2045']
+      ],
+      chartData: [
+        ['Year'],
+        ['1960'],
+        ['1965'],
+        ['1970'],
+        ['1975'],
+        ['1980'],
+        ['1985'],
+        ['1990'],
+        ['1995'],
+        ['2000'],
+        ['2005'],
+        ['2010'],
+        ['2015'],
+        ['2020'],
+        ['2025'],
+        ['2030'],
+        ['2035'],
+        ['2040'],
+        ['2045']
+      ],
+      loading: false,
+      chartOptions: {
+        chart: {
+          title: 'Yumemi challenge',
+          subtitle: 'Japan\'s prefectures population growth: 1960-2045'
+        }
+      },
+      prefectures: []
+    }
+  },
+  mounted () {
+    axios
+      .get('https://opendata.resas-portal.go.jp/api/v1/prefectures')
+      .then((response) => {
+        this.prefectures = response.data.result
+        this.chartData = this.defaultChartData
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  },
+  methods: {
+    fetchData (prefCode) {
+      return axios
+        .get('https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=' + prefCode)
+        .then((response) => {
+          var tab = []
+          const datas = response.data.result.data
+          datas.forEach(element => {
+            element.data.forEach(data => {
+              if (tab[data.year] === null || tab[data.year] === undefined
+              ) {
+                tab[data.year] = data.value
+              } else {
+                tab[data.year] += data.value
+              }
+            })
+          })
+          var finalTab = {}
+          for (let index = 0; index < tab.length; index++) {
+            if (tab[index] !== 0 && tab[index] !== undefined && tab[index] !== null) {
+              finalTab[index] = tab[index]
+            }
+          }
+          this.prefectures.forEach(element => {
+            if (element.prefCode === prefCode) {
+              finalTab.name = element.prefName
+            }
+          })
+          return finalTab
+        })
+    },
+    async showData (event, pref) {
+      this.loading = true
+      if (event.srcElement.checked) {
+        const result = await this.fetchData(pref.prefCode)
+        this.chartData[0].push(result.name)
+        const years = Object.keys(result)
+        for (let index = 0; index < years.length; index++) {
+          for (let i = 1; i < this.chartData.length; i++) {
+            if (this.chartData[i][0] === years[index]) {
+              this.chartData[i].push(result[years[index]])
+              break
+            } else if (years[index] === 'name') {
+              break
+            }
+          }
+        }
+        this.loading = false
+      } else {
+        var target = 0
+        for (let i = 0; i < this.chartData[0].length; i++) {
+          if (this.chartData[0][i] === pref.prefName) {
+            target = i
+            break
+          }
+        }
+        for (let index = 0; index < this.chartData.length; index++) {
+          this.chartData[index].splice(target, 1)
+        }
+        if (this.chartData[0].length === 1) {
+          this.chartData = this.defaultChartData
+        }
+        this.loading = false
+      }
+    }
+  },
+  computed: {
+    data () {
+      if (this.chartData[0].length > 1) {
+        return true
+      }
+      return false
+    }
   }
 }
 </script>
+<style lang="css" scoped>
+.notification {
+  position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+}
+.footer {
+   position: absolute;
+    left: 50%;
+    bottom: 0;
+    transform: translate(-50%,-50%);
+}
+</style>
